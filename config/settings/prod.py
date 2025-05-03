b24a85890080ae46bf0431cbcb014df44304e696
JonathanM-A <kamajthomas@gmail.com>
2025-05-03 19:08:45 +0000

    Dockerization

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,1 +1,22 @@
-from .base import * # noqa
+from .base import *  # noqa
+
+
+def strip_quotes(value):
+    if isinstance(value, str):
+        if (value.startswith("'") and value.endswith("'")) or (
+            value.startswith('"') and value.endswith('"')
+        ):
+            return value[1:-1]
+    return value
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": strip_quotes(env("DB_NAME")),
+        "USER": strip_quotes(env("DB_USER")),
+        "PASSWORD": strip_quotes(env("DB_PASSWORD")),
+        "HOST": strip_quotes(env("DB_HOST")),  # Uses the Docker service name
+        "PORT": env.int("DB_PORT"),
+    }
+}
